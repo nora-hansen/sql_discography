b3d8855c1e9a817b45eef7479cad0e599feca14e
norahha <norahha@stud.ntnu.no>
2023-10-06 14:04:09 +0200

    Starting on SQL db

---
+++ Discography.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS `SONGS`
+    (
+        `ID` CHAR(6) NOT NULL PRIMARY KEY,
+        `SONG_TITLE` VARCHAR(32) NOT NULL,
+        `COMPOSER` VARCHAR(32),
+        `PROJ_DATE` DATE,
+        `LAST_EXP` DATE,
+        `DAW` VARCHAR(16)
+        `ALBUM_ID` CHAR(6),
+        `IS_REMIX` BOOLEAN,
+        `IS_REMAKE` BOOLEAN,
+        `OG_ID` CHAR(6),
+        `RELEASE_ID`CHAR(6)
+    );
+
+CREATE TABLE IF NOT EXISTS `VOCALISTS`
+    (
+        `ID` CHAR(6),
+        `NAME` VARCHAR(32),
+        `SOFTWARE` VARCHAR(32),
+        `IS_AI` BOOLEAN,
+        `VOICE_PROVIDER` VARCHAR(32)
+    )
+
+CREATE TABLE IF NOT EXISTS `ALBUMS`
+    (
+        `ID` CHAR(6),
+        `TITLE` VARCHAR(32),
+        `RELEASE_DATE` DATE,
+        `RELEASE_ID` CHAR(6)
+    )
